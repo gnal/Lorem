f8fc4c3ac8fe9a0ec35e73018378e6d1796cc66a
gnal <qwertypianox@gmail.com>
2013-01-08 15:18:38 -0500

    fix fixtures

--- src/Acme/Bundle/AppBundle/DataFixtures/ORM/LoadMenuData.php
+++ src/Acme/Bundle/AppBundle/DataFixtures/ORM/LoadMenuData.php
@@ -25,53 +25,53 @@ class LoadMenuData extends AbstractFixture implements ContainerAwareInterface, O
         // ADMIN MENU
         // root
         $root = new Menu();
-        $root->createTranslations($transClass, array('fr'));
+        $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($root, array('fr'));
         $root->getTranslation()->setPublished(true)->setName('admin');
         $manager->persist($root);
         $manager->flush();
         // security
         $security = new Menu();
-        $security->createTranslations($transClass, array('fr'));
+        $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($security, array('fr'));
         $security->setParent($root);
         $security->getTranslation()->setPublished(true)->setName('Sécurité');
         $manager->persist($security);
             // users
             $menu = new Menu();
-            $menu->createTranslations($transClass, array('fr'));
+            $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($menu, array('fr'));
             $menu->getTranslation()->setRoute('@msi_user_user_admin_index');
             $menu->setParent($security);
             $menu->getTranslation()->setPublished(true)->setName('Utilisateurs');
             $manager->persist($menu);
             // groups
             $menu = new Menu();
-            $menu->createTranslations($transClass, array('fr'));
+            $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($menu, array('fr'));
             $menu->getTranslation()->setRoute('@msi_user_group_admin_index');
             $menu->setParent($security);
             $menu->getTranslation()->setPublished(true)->setName('Groupes');
             $manager->persist($menu);
         // menu
         $menu = new Menu();
-        $menu->createTranslations($transClass, array('fr'));
+        $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($menu, array('fr'));
         $menu->getTranslation()->setRoute('@msi_cmf_menu_root_admin_index');
         $menu->setParent($root);
         $menu->getTranslation()->setPublished(true)->setName('Menus');
         $manager->persist($menu);
         // content
         $content = new Menu();
-        $content->createTranslations($transClass, array('fr'));
+        $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($content, array('fr'));
         $content->setParent($root);
         $content->getTranslation()->setPublished(true)->setName('Contenu');
         $manager->persist($content);
             // pages
             $menu = new Menu();
-            $menu->createTranslations($transClass, array('fr'));
+            $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($menu, array('fr'));
             $menu->getTranslation()->setRoute('@msi_cmf_page_admin_index');
             $menu->setParent($content);
             $menu->getTranslation()->setPublished(true)->setName('Pages');
             $manager->persist($menu);
             // blocks
             $menu = new Menu();
-            $menu->createTranslations($transClass, array('fr'));
+            $this->dic->get('msi_cmf.menu_root_manager')->createTranslations($menu, array('fr'));
             $menu->getTranslation()->setRoute('@msi_cmf_page_block_admin_index');
             $menu->setParent($content);
             $menu->getTranslation()->setPublished(true)->setName('Blocs');
